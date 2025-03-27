e96c5fb57f1964b4f0b6620fa973db4bc17cc2e7
diedegysens <162812428+diedegysens@users.noreply.github.com>
2025-03-27 09:54:19 +0100

    c

---
+++ components/Navigation.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <nav class="mb-2">
+        <ul>
+            <li class="pr-2"><NuxtLink to="/books">books</NuxtLink></li>
+            <li class="pr-2"><NuxtLink to="/individuelbook">individuel book</NuxtLink></li>
+            <li class="pr-2"><NuxtLink to="/contact us">contact us</NuxtLink></li>
+            <li class="pr-2"><NuxtLink to="/signin">sign in</NuxtLink></li>
+
+
+        </ul>
+        </nav>
+    </div>
+</template>
+
+<style>
+li{ 
+    display: block;
+}
+</style>
